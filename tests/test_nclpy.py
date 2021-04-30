e0b0a471060799e636f293fc66ffce527d669bfc
Hannah Rubin <hannahrubin100@gmail.com>
2021-04-30 11:54:44 -0400

    removed test

--- tests/test_nclpy.py
+++ tests/test_nclpy.py
@@ -2,20 +2,3 @@
 
 """Tests for `nclpy` package."""
 
-
-# import unittest
-
-# from nclpy import nclpy
-
-
-# class TestNclpy(unittest.TestCase):
-#     """Tests for `nclpy` package."""
-
-#     def setUp(self):
-#         """Set up test fixtures, if any."""
-
-#     def tearDown(self):
-#         """Tear down test fixtures, if any."""
-
-#     def test_000_something(self):
-#         """Test something."""
